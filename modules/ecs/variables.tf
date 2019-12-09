c259462ac39c559935c38a3d3ff4964f7556556d
David <dalau6@gmail.com>
2019-12-08 21:53:18 -0800

    added ecs variables.

---
+++ modules/ecs/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "region" {
+  type        = string
+  description = "The region for AWS assets"
+}
